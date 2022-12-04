047ae1db2f64e508c1e28d9553ef140046fecada
[carlos martinez] <theycallhellp@gmail.com>
2022-12-04 12:18:41 -0800

    about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
+import About from './components/About'
 
 function App() {
   // state that determines which portion of content is loaded at the bottom of the page
   const [currentContent, setCurrentContent] = useState('about');
-
+  function returnContent() {
+    if (currentContent === 'about') {
+      return <About />
+    } else if (currentContent === 'projects') {
+      return <h2>Projects page</h2>
+      // return <Projects />
+    } else if (currentContent === 'contact') {
+      return <h2>Contact page</h2>
+      // return <Contact />
+    } else if (currentContent === 'resume') {
+      return <h2>Resume page</h2>
+      // return <Resume />
+    }
+  }
   // We return the Welcome component and pass loggedIn and setLoggedIn as props.
-  return <Header currentContent={currentContent} setCurrentContent={setCurrentContent} />;
+  return (
+    <>
+      <Header currentContent={currentContent} setCurrentContent={setCurrentContent} />;
+      <main>
+        {returnContent()}
+      </main>
+    </>
+  )
 }
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import photo from '../images/tempSelfPic.jpg'
 
 export default function About() {
     return (
-      <h2>About page</h2>
+    <>
+      <h2>Carlos Martinez</h2>
+      <img src={photo} alt='carlos' className='rounded-circle avatar-img'/>
+      <p>Some about me stuff I can fil out at a later date. Lorem ipsum dolor sit amet, consd ectetur adipiscing elit. Ut porttitor rutrum sapien, sit amet facilisis ipsum hendrerit id. Vestibulum non erat vel risus tincidunt iaculis non et tortor. Morbi dolor diam, egestas id est id, interdum maximus elit. Cras nec consectetur lectus. Morbi a dictum magna. Donec posuere pulvinar ante, vel lacinia ante rutrum vitae. Maecenas at metus rhoncus, pretium odio ac, pharetra lectus. Donec a orci ipsum. Suspendisse condimentum velit id elit venenatis pellentesque. Suspendisse diam elit, molestie nec maximus sed, elementum et velit.</p>
+    </>
     );
 }
