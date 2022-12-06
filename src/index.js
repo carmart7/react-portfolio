c5ef1f313590da94052842089ec65dcd1e8ac6ac
[carlos martinez] <theycallhellp@gmail.com>
2022-12-05 18:23:03 -0800

    service workers and install functionality

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { createRoot}from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/styles.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />)
-// ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
+
+serviceWorkerRegistration.unregister();
